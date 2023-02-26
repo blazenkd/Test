09cd6436acc74482dda529498f583d5ea37a72a9
Kevin Duong <kcduong.py@gmail.com>
2023-02-26 00:36:47 -0800

    Commit

---
+++ Projects/FCC_Pandas_RLE.py/Exercise_1.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+sales = pd.read_csv(
+    'data/sales_data.csv',
+    parse_dates=['Date'])
+
+sales.head()
+
+print(sales.head())
