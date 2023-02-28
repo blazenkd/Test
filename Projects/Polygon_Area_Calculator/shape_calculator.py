1095e225d171677b325812d2bc43215fd9b6a9cd
Kevin Duong <kcduong.py@gmail.com>
2023-02-27 23:05:02 -0800

    Commit

--- Projects/Polygon_Area_Calculator/shape_calculator.py
+++ Projects/Polygon_Area_Calculator/shape_calculator.py
@@ -1,197 +1,238 @@
 class Rectangle:
-    """Represents rectangular shapes.
+    '''
+    Represents a rectangle.
 
-    The Rectangle object is initialized with width and height of the rectangular
-    shape. Area, perimeter, diagonal and the picture of the shape methods will
-    be included.
+    The Rectangle class initializes a rectangular shape with a given width and height. 
+    It provides methods to compute the area, perimeter, diagonal, and picture of the rectangle.
 
     Attributes
     ----------
     width : int
-        Width of the rectangle.
+        The width of the rectangle.
     height : int
-        Height of the rectangle.
-    """
-
+        The height of the rectangle.
+    '''
     def __init__(self, width, height):
-        """Initializes the Rectangle object.
+        '''
+        Initializes a new Rectangle object with the given width and height.
 
         Parameters
         ----------
         width : int
-            An integer that represents the width of the shape.
+            The width of the rectangle.
         height : int
-            An integer that represents the height of the shape.
-        """
+            The height of the rectangle.
+        '''
         self.width = width
         self.height = height
 
     def set_width(self, width):
-        """Sets width of the Rectangle object.
+        '''
+        Sets the width of the Rectangle object.
 
         Parameters
         ----------
         width : int
-            Width of the Rectangle object.
-        """
+            The desired width of the Rectangle object.
+        '''
         self.width = width
 
     def set_height(self, height):
-        """Sets height of the Rectangle object.
+        '''
+        Sets the height of the Rectangle object.
 
         Parameters
         ----------
         height : int
-            Height of the Rectangle object
-        """
+            The desired height of the Rectangle object.
+        '''
         self.height = height
 
     def get_width(self):
-        """Gets width of the Rectangle object.
+        '''
+        Returns the width of the Rectangle object.
 
         Returns
         -------
         int
-            Width of the object.
-        """
+            Width of the Rectangle object.
+        '''
         return self.width
 
     def get_height(self):
-        """Gets height of the Rectangle object.
+        '''
+        Returns the height of the Rectangle object.
 
         Returns
         -------
         int
-            Height of the object.
-        """
+            Height of the Rectangle object.
+        '''
         return self.height
 
     def get_area(self):
-        """Gets the area of the Rectangular object.
+        '''
+        Calculates the area of the rectangle.
 
         Returns
         -------
         int
-            Area given by width * height.
-        """
-        return self.width * self.height
+            The area of the rectangle, given by multiplying the width by the height.
+        '''
+        return (self.width * self.height)
 
     def get_perimeter(self):
-        """Gets the perimeter of the Rectangular object.
+        '''
+        Returns the perimeter of the rectangle.
 
         Returns
         -------
         int
-            Perimeter given by (2 * width) + (2 * height).
-        """
-        return (2 * self.width) + (2 * self.height)
+            The perimeter of the rectangle, which is calculated as 2 times the width plus 2 times the height.
+        '''
+        return (2 * self.width + 2 * self.height)
 
     def get_diagonal(self):
-        """Gets the diagonal of the Rectangular object.
+        '''
+        Computes the diagonal of the Rectangle object using the Pythagorean theorem.
 
         Returns
         -------
         float
-            Diagonal given by (width ** 2 + height ** 2) ** 0.5.
-        """
-        return (self.width ** 2 + self.height ** 2) ** 0.5
+            The length of the diagonal of the rectangle.
+        '''
+        return ((self.width ** 2 + self.height ** 2) ** .5)
 
     def get_picture(self):
-        """Draws the shape of the Rectangular object using '*' characters.
+        '''Returns a string that represents the shape using lines of "*".
 
-        The number of lines drawn is equal to the height of the object.
-        The number of '*' in each line is equal to the width of the object.
+        The number of lines should be equal to the height and the number of "*" in each line should be equal to the width.
+        There should be a new line (\n) at the end of each line.
+        If the width or height is larger than 50, this should return the string: "Too big for picture.".
 
         Returns
         -------
         str
-            Shape (picture) of the object if height or width are less than
-            or equal to 50
-
-            Error  message otherwise.
-        """
-        if (self.width > 50):
+            A string that represents the shape using lines of "*".
+        '''
+        if self.width > 50 or self.height > 50:
             return "Too big for picture."
+        else:
+            picture = ""
+            for i in range(self.height):
+                picture += "*" * self.width + "\n"
+            return picture
 
-        if (self.height > 50):
-            return "Too big for picture."
-        
-        line = '*' * self.width # Number of '*' in each line
-
-        lines = [line for _ in range(self.height)] # Number of lines in shape
-
-        picture = '\n'.join(lines)
-
-        return picture + "\n"
-
-    def get_amount_inside(self, shape):
-        """Determines number of times a 'shape' fits into the Rectangle object.
+    def get_amount_inside(self, other):
+        '''
+        Calculates the number of times a given shape (square or rectangle) can fit
+        inside the current rectangle object without rotation.
 
         Parameters
         ----------
-        shape : obj
-            An instance of a Rectangular of Square class.
+        other : object
+            Another rectangle or square object to be placed inside the current rectangle.
 
         Returns
         -------
         int
-            Number of times the 'shape' fits inside this object.
-        """
-        w = self.width // shape.width
-        h = self.height // shape.height
-
-        return w * h
-
-    def __str__(self):
-        """String representation of the Rectangle object.
+            The number of times the given shape can fit inside the current rectangle.
+        '''
         
+        if self.width < other.width or self.height < other.height:
+            return 0
+        
+        width_ratio = self.width // other.width
+        height_ratio = self.height // other.height
+        
+        return width_ratio * height_ratio
+
+    def __repr__(self):
+        '''
+        Returns a string representation of the Rectangle object that can be used
+        to recreate the object.
+
         Returns
         -------
         str
-            A string giving the 'width' and 'height' of the object.
-        """
-        return "Rectangle(width=" + str(self.width) + ", height=" + str(self.height) + ")"
+            String representation of the Rectangle object.
+        '''
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 class Square(Rectangle):
-    """Represents square shapes.
+    '''
+    A subclass of the Rectangle class representing a square shape. 
 
-    The square object is initialized with a side length of the sqare shape.
+    The Square object is initialized with a single side length value. 
+    It inherits the properties of the Rectangle class, including the 
+    methods to calculate the area, perimeter, diagonal, and the picture
+    of the shape.
 
     Attributes
     ----------
     side : int
-        Side length of the square.
-    """
+        The length of a side of the square.
 
+    Methods
+    -------
+    __init__(side)
+        Initializes the Square object with a given side length.
+    __repr__()
+        Returns a string representation of the Square object.
+    get_picture()
+        Returns a string representation of the Square object as a picture made of '*'.
+    '''
     def __init__(self, side):
-        """Initializes the Square object.
+        """Creates a Square object with a given side length.
 
         Parameters
         ----------
         side : int
-            An integer of side length of the square.
-        
+            The length of the square's side.
         """
         self.width = side
-        self.height = side
+        self.height = side    
 
     def set_side(self, side):
-        """Sets the sides of the Square object.
+        """Set the side length of the Square object.
 
-        Parameter
-        ---------
+        Parameters
+        ----------
         side : int
-            Side length of the square.
+            An integer representing the length of the sides of the square.
         """
         self.width = side
         self.height = side
 
-    def __str__(self):
-        """String representation of the Square object.
+    def __repr__(self):
+        '''
+        Returns a string representation of the Square object that can be used
+        to recreate the object.
 
         Returns
         -------
         str
-            A string giving the side of the Square object.
-        """
-        return "Square(side=" + str(self.width) + ")"
+            String representation of the Square object.
+        '''
+        return f"Square(side={self.width})"
+
+# Testing Area
+# rect = Rectangle(5, 10)
+# print("rect width:", rect.width)
+# print("rect height:", rect.height)
+# rect.set_width(6)
+# rect.set_height(11)
+# print("Get width:",rect.get_width())
+# print("Get Height:", rect.get_height())
+# print("Get Area:", rect.get_area())
+# print("Get Perimeter:", rect.get_perimeter())
+# print("Get Picture:", rect.get_picture())
+# print("Get Amount Inside:", rect.get_amount_inside(Rectangle(2,2)))
+# print(rect)
+# square = Square(4)
+# print("Square width:", square.width)
+# print("Square height:", square.height)
+# square.set_side(5)
+# print("Square width:", square.width)
+# print("Square height:", square.height)
+# print(square)
