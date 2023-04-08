f80d6d7744e2e9d31e3194b3253776ec12fc585b
Kevin Duong <kcduong.py@gmail.com>
2023-04-07 23:20:26 -0700

    updates

---
+++ Fundamentals/C/conditionals-and-loops/for-loop.c
@@ -0,0 +1,91 @@
+/*
+The for Loop 
+The for statement is a loop structure that executes statements a fixed number of times. 
+
+It takes the form: 
+
+    for (initvalue; condition; increment) {
+    statements;
+    }
+
+The initvalue is a counter set to an initial value. This part of the for loop is performed only once.
+
+The condition is a Boolean expression that compares the counter to a value before each loop iteration,
+stopping the loop when false is returned.
+
+The increment increases (or decreases) the counter by a set value.
+
+For example, the program below displays 0 through 9:
+*/
+
+#include <stdio.h>
+
+int single_digit() {
+    int i;
+    int max = 10;
+  
+    for (i = 0; i < max; i++) {
+        printf("%d\n", i);
+    }
+}
+
+/*
+
+The for loop can contain multiple expressions separated by commas in each part. 
+
+For example:
+
+    for (x = 0, y = num; x < y; i++, y--) { 
+    statements; 
+    }
+
+Also, you can skip the initvalue, condition and/or increment.
+
+For example:
+*/
+
+int func() {
+    int i=0;
+    int max = 10;
+  
+    for (;i < max; i++) {
+        printf("%d\n", i);
+    }
+}
+
+/*
+Loops can also be nested.
+
+When writing a program this way, there is an outer loop and an inner loop.
+For each iteration of the outer loop the inner loop repeats its entire cycle.
+
+In the following example, nested for loops are used to output a multiplication table:
+*/
+
+int func_2() {
+    int i, j;
+    int table = 10;
+    int max = 12;
+  
+    for (i = 1; i <= table; i++) {
+        for (j=0; j <= max; j++) {
+            printf("%d x %d = %d\n", i, j, i*j);
+        }
+        printf("\n"); /* blank line between tables */
+    }
+    
+    return 0;
+}
+
+/*
+A break in an inner loop exits that loop and execution continues with the outer loop.
+
+A continue statement works similarly in nested loops.
+*/
+
+int main() {
+    single_digit();
+    func();
+    func_2();
+    return 0;
+}
--- Fundamentals/C/conditionals-and-loops/while-loop.c
+++ Fundamentals/C/conditionals-and-loops/while-loop.c
@@ -23,7 +23,7 @@ Run the code and see how it works!
 
 #include <stdio.h>
 
-int count() {
+int count_1() {
   int count = 1;
   
   while (count < 8) {
@@ -52,8 +52,91 @@ For example:
 
 */
 
+// How many numbers will the follwoing code output?
+int count_2() {
+  int count = 1;
+
+  do {
+
+    printf("%d\n", count);
+
+    count++;
+
+  } while (count < 8);
+}
+
+/*
+break and continue 
+
+The break statement was introduced for use in the switch statement.
+It is also useful for immediately exiting a loop.
+For example, the following program uses a break to exit a while loop:
+*/
+
+int count_3() {
+    int num = 5;
+  
+    while (num > 0) {
+        if (num == 3)
+            break;
+        printf("%d\n", num);
+        num--;
+    } 
+    
+    return 0;
+} 
+
+/*
+This program displays:
+
+5
+4
+
+and then exits the loop.
+
+When you want to remain in the loop, but skip ahead to the next iteration,
+you use the continue statement.
+
+For example:
+*/
+
+int count_4() {
+    int num = 5;
+  
+    while (num > 0) {
+    num--;
+    if (num == 3)
+      continue;
+      
+    printf("%d\n", num);
+  } 
+}
+
+/*
+The program output displays:
+
+4
+
+2
+
+1
+
+0
+
+As you can see, the value 3 is skipped.
+
+In the code above, if num was decremented after the
+continue statement an infinite loop would be created.
+
+Although the break and continue statements can be convenient,
+they should not be a substitute for a better algorithm.
+*/
+
 // Run main function
 int main() {
-    count();
+    count_1();
+    count_2();
+    count_3();
+    count_4();
     return 0;
 }
