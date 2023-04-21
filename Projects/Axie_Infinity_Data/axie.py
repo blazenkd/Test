33584a3b42327317a2409a74d4a5cd32a866c4bd
Kevin Duong <kcduong.py@gmail.com>
2023-04-20 23:52:17 -0700

    updates

--- Projects/Axie_Infinity_Data/axie.py
+++ Projects/Axie_Infinity_Data/axie.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = "https://api-gateway.skymavis.com/x/origin/battle-history?type=pvp&client_id=1ec9eb7e-579e-6a97-a60c-dd46969f474e&limit=5&page=1"
 
@@ -9,4 +10,9 @@ headers = {
 
 response = requests.get(url, headers=headers)
 
-print(response.text)
+def print_all_keys(response):
+    json_data = json.loads(response.text)
+    for key in json_data.keys():
+        print(key)
+
+print_all_keys(response)
